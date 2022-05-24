75c57bd1440a49d72d020edbb668c38ac04e3d0f
DupontJeanBaptiste <jbdupont67@gmail.com>
2022-05-24 14:55:56 +0200

    update quete 9

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -10,32 +10,22 @@ use Doctrine\Persistence\ObjectManager;
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
     public const PROGRAM = [
-        'The lord of the ring' => ['summary' => 'L\'anneau, bla bla bla.'], 
-        'Star Wars' => ['summary' => 'Luc je suis ton père et tout et tout'],
-        'The ring' => ['summary' => 'Je ne pourrai pas vous dire, j\'ai pas regardé'],
-        'Fast and furious' => ['summary' => 'Des voitures et des muscles'],
-        'Nom de film romantic random' => ['summary' => 'aled.'],
+        'The lord of the ring' => ['summary' => 'L\'anneau, bla bla bla.', 'category' => 'Fantasy'], 
+        'Star Wars' => ['summary' => 'Luc je suis ton père et tout et tout', 'category' => 'SF'],
+        'The ring' => ['summary' => 'Je ne pourrai pas vous dire, j\'ai pas regardé', 'category' => 'Horreur'],
+        'Fast and furious' => ['summary' => 'Des voitures et des muscles', 'category' => 'Action'],
+        'Nom de film romantic random' => ['summary' => 'aled.', 'category' => 'Romance'],
     ];
-    public const CATEGORY = [
-        'Fantasy',
-        'SF',
-        'Horreur',
-        'Action',
-        'Romance',
-    ];
+    
     public function load(ObjectManager $manager): void
     {
-        $i = 0;
-        foreach (self::PROGRAM as $key => $programName) {
+        foreach (self::PROGRAM as $programName => $infos) {
             $program = new Program();
-            $program->setTitle($key);
-            foreach ($programName as $summary => $value) {
-            $program->setSummary($value);
-            $program->setCategory($this->getReference('category_'.self::CATEGORY[$i]));
-            $i = $i + 1;
+            $program->setTitle($programName);
+            $program->setSummary($infos['summary']);
+            $program->setCategory($this->getReference('category_'.$infos['category']));
             $manager->persist($program);
             }
-        }
 
             $manager->flush();
     }
