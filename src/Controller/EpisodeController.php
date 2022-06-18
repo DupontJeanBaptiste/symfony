5aff1868aa46d33cf37be5f3169b10db021e35d3
DupontJeanBaptiste <jbdupont67@gmail.com>
2022-06-18 15:57:49 +0200

    push

---
+++ src/Controller/EpisodeController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Episode;
+use App\Form\EpisodeType;
+use App\Repository\EpisodeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+#[Route('/episode')]
+class EpisodeController extends AbstractController
+{
+    #[Route('/', name: 'app_episode_index', methods: ['GET'])]
+    public function index(EpisodeRepository $episodeRepository, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        return $this->render('episode/index.html.twig', [
+            'episodes' => $episodeRepository->findAll(),
+            'categories' => $categoryRepository->findAll(),
+            'programs' => $programRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_episode_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EpisodeRepository $episodeRepository, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $episode = new Episode();
+        $form = $this->createForm(EpisodeType::class, $episode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $episodeRepository->add($episode, true);
+
+            return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('episode/new.html.twig', [
+            'episode' => $episode,
+            'form' => $form,
+            'categories' => $categoryRepository->findAll(),
+            'programs' => $programRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_episode_show', methods: ['GET'])]
+    public function show(Episode $episode, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        return $this->render('episode/show.html.twig', [
+            'episode' => $episode,
+            'categories' => $categoryRepository->findAll(),
+            'programs' => $programRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_episode_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Episode $episode, EpisodeRepository $episodeRepository, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $form = $this->createForm(EpisodeType::class, $episode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $episodeRepository->add($episode, true);
+
+            return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('episode/edit.html.twig', [
+            'episode' => $episode,
+            'form' => $form,
+            'categories' => $categoryRepository->findAll(),
+            'programs' => $programRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_episode_delete', methods: ['POST'])]
+    public function delete(Request $request, Episode $episode, EpisodeRepository $episodeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$episode->getId(), $request->request->get('_token'))) {
+            $episodeRepository->remove($episode, true);
+        }
+
+        return $this->redirectToRoute('app_episode_index',
+        [
+        ], Response::HTTP_SEE_OTHER);
+    }
+}
